5999c277eb2b8d68a5fcfb0b44aa8259605d6f92
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-07-23 14:28:29 -0400

    adding middleware

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,7 +1,17 @@
 const routes = (app) => {
   app
     .route("/contact")
-    .get((req, res) => res.send("GET request successful!"))
+    .get(
+      (req, res, next) => {
+        //middleware
+        console.log(`Request from ${req.originalUrl}`);
+        console.log(`Request type ${req.method}`);
+        next();
+      },
+      (req, res, next) => {
+        res.send("GET request successful!");
+      }
+    )
     .post((req, res) => res.send("POST request successful!"));
 
   app
