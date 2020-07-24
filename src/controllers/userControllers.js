92a89bfde620ec1ffa66221432c953d8cb799895
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-07-24 12:56:41 -0400

    handlers for login

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -12,3 +12,51 @@ export const loginRequire = (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized user!" });
   }
 };
+
+export const register = (req, res) => {
+  const newUser = new User(req.body);
+  //On the request body there will be a hashPassword and we will encrypt with "bcrypt" the password
+  // to compare with the password at the DB. 10 is the number the hash require to encrypt
+  newUser.hashPassword = bcrypt.hashSync(req.body.password, 10);
+  newUser.save((err, user) => {
+    if (err) {
+      return res.status(400).send({
+        message: err,
+      });
+    } else {
+      //   If there is no error we want to set the password to undefined, not sending the password
+      // as a response to avoid any attack or exposure
+      user.hashPassword = undefined;
+      return res.json(user);
+    }
+  });
+};
+
+export const login = (req, res) => {
+  User.findOne(
+    {
+      email: req.body.email,
+    },
+    (err, user) => {
+      if (err) throw err;
+      if (!user) {
+        res
+          .status(401)
+          .json({ message: "Authentication failed. No user found!" });
+      } else if (user) {
+        if (!user.comparePassword(req.body.password, user.hashPassword)) {
+          res
+            .status(401)
+            .json({ message: "Authentication failed. Wrong password" });
+        } else {
+          return res.json({
+            token: jwt.sign(
+              { email: user.email, username: user.username, _id: user.id },
+              "RESTFULAPIs"
+            ),
+          });
+        }
+      }
+    }
+  );
+};
