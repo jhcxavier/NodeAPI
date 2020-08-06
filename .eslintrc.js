5e1382aaed79113bf96e9fd6439292d7d77c5a9f
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-08-06 14:11:27 -0400

    adding automated nodescan

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports={
+    "parseOptions":{
+        "ecmaVersion":"6",
+        "sourceType":"module"
+    },
+    "env":{
+        "node":true,
+        "es6":true
+    },
+    "rules":{
+        "no-empty":"error",
+        "no-multiple-empty"
+    }
+}
