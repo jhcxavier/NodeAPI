73c08e5cfbb419637bc35efbf63ec6c46296458f
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-08-06 15:44:46 -0400

    configuring eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
-module.exports={
-    "parseOptions":{
-        "ecmaVersion":"6",
-        "sourceType":"module"
-    },
-    "env":{
-        "node":true,
-        "es6":true
-    },
-    "rules":{
-        "no-empty":"error",
-        "no-multiple-empty"
-    }
-}
+module.exports = {
+  parseOptions: {
+    ecmaVersion: "6",
+    sourceType: "module",
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  rules: {
+    "no-empty": "error",
+    "no-multiple-empty": "warn",
+    "no-var": "error",
+    "prefer-const": "error",
+  },
+};
