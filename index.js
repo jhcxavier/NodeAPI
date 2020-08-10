7c11cb19182f854d06fd8d086853087af5d1080d
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-08-10 14:43:10 -0400

    updating domain

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(xss());
 
 //Enabling Cors
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
