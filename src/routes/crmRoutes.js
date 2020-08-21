7d97e08d18bc297a827e8adf134d3cc242f9bbdc
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-08-21 17:49:32 -0400

    updating

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -19,18 +19,18 @@ const routes = (app) => {
         console.log(`Request type ${req.method}`);
         next();
       },
-      // loginRequire,
+      loginRequire,
       getContact
     )
     //post endpoint
-    .post(addNewContact);
+    .post(loginRequire, addNewContact);
 
   app
     .route("/contact/:contactID")
     //get specific contact
     .get(loginRequire, getContactById)
     //Update contact
-    .put(updateContact)
+    .put(loginRequire, updateContact)
     //delete contact
     .delete(loginRequire, deleteContact);
 
